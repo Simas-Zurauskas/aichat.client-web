86111620fc7028f4f644c550c33d566cd643c92f
Simas <simaszurauskas@gmail.com>
2025-01-18 13:16:42 +0200

    dev

--- src/screens/AuthPage/AuthPage.tsx
+++ src/screens/AuthPage/AuthPage.tsx
@@ -30,6 +30,17 @@ const Main = styled.main`
   }
 `;
 
+export function isInAppBrowser() {
+  if (typeof navigator !== 'undefined') {
+    // @ts-ignore
+    const ua = navigator.userAgent || navigator.vendor || window.opera;
+    // Basic checks for some common in-app browsers:
+    const inAppKeywords = ['FBAN', 'FBAV', 'Instagram', 'LinkedInApp', 'Twitter'];
+    return inAppKeywords.some((k) => ua.includes(k));
+  }
+  return false;
+}
+
 const AuthPage = () => {
   const params = useSearchParams();
   const { setUser } = useActionsAuth();
@@ -58,19 +69,17 @@ const AuthPage = () => {
 
   return (
     <Main>
-      {/* <GameOfLife /> */}
       <div className="waves">
         <Waves />
       </div>
-
-      {/* <Button variant="outlined" onClick={handleSignIn}>
-        <GoogleIcon fontSize="small" sx={{ mr: 1 }} />
-        Continue with Google++
-      </Button> */}
-
-      <a href={generateGoogleOAuthUrl()} target="_blank" rel="noopener noreferrer">
-        Open in your default browser
-      </a>
+      {isInAppBrowser() ? (
+        <h4>Not suported</h4>
+      ) : (
+        <Button variant="outlined" onClick={handleSignIn}>
+          <GoogleIcon fontSize="small" sx={{ mr: 1 }} />
+          Continue with Google
+        </Button>
+      )}
 
       <div className="waves wawes--top">
         <Waves />
